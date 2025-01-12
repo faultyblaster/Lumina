f05d876bbbb4cdd5bea270c329e2387474a0af23
Faulty Blaster <faultyblaster@gmail.com>
2025-01-12 03:15:27 -0600

    Update main.rs

--- Rust/RustByExample/ex02_primitives/src/main.rs
+++ Rust/RustByExample/ex02_primitives/src/main.rs
@@ -1,3 +1,12 @@
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (int_param, bool_param) = pair;
+    (bool_param, int_param)
+}
+
+// The following struck is for the activity.
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
 fn main() {
     //
     // Primitives
@@ -60,4 +69,42 @@ fn main() {
 
     // Use underscores to improve readability
     println!("One million is written as {}", 1_000_000);
+
+    //
+    // Tuples
+    //
+
+    let long_tuple = (
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+
+    // Values can be extracted for the tuple using tuple indexing.
+    println!("Long tuple frist value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
+
+    // Tuples can be tuples members:
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), (-2i16));
+
+    // But long tuples (more than 12 elements cannot be printed)
+    let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("Too long tuple: {:?}", too_long_tuple);
+
+    let pair = (1, true);
+    println!("Pair is {:?}", pair);
+
+    println!("The reversed pair is {:?}", reverse(pair));
+
+    // To create one element tuples, the comma is required to tell them appart\
+    // from a literal surrounded by parentheses
+    println!("One element tuple: {:?}", (5u32,));
+    println!("Just an integer: {:?}", (5u32));
+
+    // Tuples can be deconstructed to create bindings.
+    let tuple = (1, "hellow", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
 }
