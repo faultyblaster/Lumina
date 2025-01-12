03286070bd35ce3c84f174d014f316b42349b781
Faulty Blaster <faultyblaster@gmail.com>
2025-01-12 02:33:20 -0600

    2.1 Literals and operators

--- Rust/RustByExample/ex02_primitives/src/main.rs
+++ Rust/RustByExample/ex02_primitives/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
+    //
+    // Primitives
+    //
+
     // Variables can be type anotated.
     let logical: bool = true;
 
@@ -21,11 +25,39 @@ fn main() {
     // mutable = true;
 
     let mutable = true;
-    
-    // Array signature consists of type T amd Lengetas [T; length]
-    let my_array: [i32; 5] =[1,2,3,4,5];
 
-    // Tuple is a collection of values of different types 
+    // Array signature consists of type T amd Lengetas [T; length]
+    let my_array: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // Tuple is a collection of values of different types
     // and is constructed using parentheses ().
     let my_tuple = (5u32, 1u8, true, -5.04f32);
+
+    //
+    // Literals and operators
+    //
+
+    // Integer addition:
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Integer substraction:
+    println!("1- 2 = {}", 1i32 - 2);
+
+    // Scientific notation:
+    println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
+
+    //short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 < 5);
+    println!("0x80 >> 2 is {}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability
+    println!("One million is written as {}", 1_000_000);
 }
